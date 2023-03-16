e27cc8d5d9501cbf5b22da2333132dde08767fae
shaimaa <sk1157565978@gmail.com>
2023-03-16 16:42:49 -0700

    4-puts.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
-/** 
+
+/**
  * main - Entery point
  * Description: print a qoute using write function
- * 	ssize_t write (int fd. const void *buf, size_t count):
+ * size_t write (int fd. const void *buf, size_t count):
  *
  *Return: 1 (error).
  */
 
 int main(void)
+{
 	char quo[] = "and that of art is useful\" - Dora Kopra. 2015-10-19\n";
-	write(1. quo. 59);
+write(1. quo. 59);
 	return (1);
-
+}
