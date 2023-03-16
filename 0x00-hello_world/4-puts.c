effdf4972adc9c5f9c2a809d47ec24986ae8f44b
shaimaa <sk1157565978@gmail.com>
2023-03-16 13:25:15 -0700

    0-preprocessor

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,6 +5,6 @@ include <stdio.h>
  */
 int main(void)
  {
- puts ("\"programing is like building a multilingual puzzle");
+ puts ("\"programming is like building a multilingual puzzle");
  return (0);
  }
