c0b59e351095f5613bbbcb5771eaa1ee98ae176e
shaimaa <sk1157565978@gmail.com>
2023-03-16 13:24:01 -0700

    0-preprocessor

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,8 @@ include <stdio.h>
  * main - A program that prints a line with puts function
  * Return: @ (success)
  */
-  int main(void)
+int main(void)
  {
- puts ("/"programing is like building a multilingual puzzle");
+ puts ("\"programing is like building a multilingual puzzle");
  return (0);
  }
-
